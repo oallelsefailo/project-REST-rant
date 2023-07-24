c5066aba05392038904b32d605824dda8fafbf54
Brandon <oallelsefailo@gmail.com>
2023-07-23 20:15:10 -0700

    Centered the 404 page

--- views/Default.jsx
+++ views/Default.jsx
@@ -4,7 +4,7 @@ function Def(html) {
   return (
     <html>
       <head>
-        <title>Title</title>
+        <title>REST-Rant</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"/>
         <link rel="stylesheet" href="/css/style.css"/>
       </head>
--- views/error404.jsx
+++ views/error404.jsx
@@ -5,11 +5,11 @@ function error404() {
   return (
     <Def>
       <main>
-        <h1>404: PAGE NOT FOUND</h1>
-          <div>
-            <img src="/images/kitty-404.jpg" alt="Kitty 404 Error" />
-          </div>
-        <p>Oops, sorry, we can't find this page!</p>
+        <h1 className="text-center">404: PAGE NOT FOUND</h1>
+        <div className="d-flex justify-content-center align-items-center flex-column">
+          <img src="/images/kitty-404.jpg" alt="Kitty 404 Error" />
+          <p className="text-center">Oops, sorry, we can't find this page!</p>
+        </div>
       </main>
     </Def>
   );
