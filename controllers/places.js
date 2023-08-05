71fdec748b749b9a1c68ac158bfdf6c085a6daf3
Brandon <oallelsefailo@gmail.com>
2023-08-05 11:14:28 -0700

    Last part done i think

--- controllers/places.js
+++ controllers/places.js
@@ -11,6 +11,7 @@ router.get("/", (req, res) => {
       res.render("error404");
     });
 });
+
 router.post("/", (req, res) => {
   db.Place.create(req.body)
     .then(() => {
@@ -21,12 +22,16 @@ router.post("/", (req, res) => {
       res.render("error404");
     });
 });
+
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
+
 router.get("/:id", (req, res) => {
   db.Place.findById(req.params.id)
+    .populate("comments")
     .then((place) => {
+      console.log(place.comments);
       res.render("places/show", { place });
     })
     .catch((err) => {
@@ -34,6 +39,7 @@ router.get("/:id", (req, res) => {
       res.render("error404");
     });
 });
+
 
 router.put("/:id", (req, res) => {
   res.render("PUT /places/:id stub");
@@ -50,6 +56,27 @@ router.delete("/:id", (req, res) => {
     });
 });
 
+  router.post("/:id/comment", (req, res) => {
+    console.log(req.body);
+    db.Place.findById(req.params.id)
+      .then((place) => {
+        db.Comment.create(req.body)
+          .then((comment) => {
+            place.comments.push(comment.id);
+            place.save().then(() => {
+              res.redirect(`/places/${req.params.id}`);
+            });
+          })
+          .catch((err) => {
+            res.render("error404");
+          });
+      })
+      .catch((err) => {
+        res.render("error404");
+      });
+  });
+
+
 router.get("/:id/edit", (req, res) => {
   res.send("GET edit form stub");
 });
@@ -59,4 +86,5 @@ router.post("/:id/rant", (req, res) => {
 router.delete("/:id/rant/:rantId", (req, res) => {
   res.send("GET /places/:id/rant/:rantId stub");
 });
+
 module.exports = router;
--- models/places.js
+++ models/places.js
@@ -9,9 +9,11 @@ const placeSchema = new mongoose.Schema({
   founded: {
     type: Number,
     min: [1673, "Surely not that old?!"],
-    max: [new Date().getFullYear(), "Hey, this year is in the future!"],
+    max: [new Date().getFullYear(), "This is the future!"],
   },
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
 });
+
 
 placeSchema.methods.showEstablished = function () {
   return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`;
